6f777b74eb62732ef2a38858dd9206d25f31a56f
been <ideal402@naver.com>
2024-10-26 13:51:36 +0900

    [feat] 로그인 토큰 발행 추가

--- routes/user.api.js
+++ routes/user.api.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userController = require("../controllers/user.controller");
 
 router.post("/", userController.createUser);
-router.post("/login", userController.login);
 
 
 module.exports = router;
