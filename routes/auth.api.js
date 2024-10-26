b2faab087fe72261c54613da3c2535a41dd0fb2f
been <ideal402@naver.com>
2024-10-27 02:11:08 +0900

    [fix] 라우터 재설정

--- routes/auth.api.js
+++ routes/auth.api.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
-const userController = require("../controllers/user.controller");
 
 router.post("/login", authController.login);
-router.get("/me", authController.authenticate, userController.gerUser);
 
 module.exports = router;
