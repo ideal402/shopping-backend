3c823d1fe6613dfb2c524a3d3febcf7e099b1921
ideal402 <sksekdh@gmail.com>
2024-11-10 01:56:56 +0900

    [feat] 구글 로그인 추가

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,8 @@ const authController = {};
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../model/User");
+const { OAuth2Client } = require("google-auth-library");
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 require("dotenv").config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
@@ -29,6 +31,35 @@ authController.login = async (req, res) => {
   }
 };
 
+authController.google = async (req, res) => {
+  try {
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+
+    const { email, name } = ticket.getPayload();
+
+    let user = await User.findOne({ email: email });
+    if (!user) {
+      const randomPassword = generateRandomPassword(10);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({ email: email, password: newPassword, name: name });
+      await user.save();
+    }
+
+    const sessionToken = await user.generateToken();
+
+    return res.status(200).json({ status: "success", user, sessionToken });
+
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 authController.authenticate = async (req, res, next) => {
   try {
     const tokenString = req.headers.authorization;
@@ -49,17 +80,28 @@ authController.authenticate = async (req, res, next) => {
   }
 };
 
-authController.checkAdmin = async(req,res,next) =>{
+authController.checkAdmin = async (req, res, next) => {
   try {
     const userId = req.userId;
     const user = await User.findById(userId);
-    
-    if(user.level !== "admin") throw new Error("no permission");
 
-    next()
-    
+    if (user.level !== "admin") throw new Error("no permission");
+
+    next();
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
+};
+
+function generateRandomPassword(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomPassword = "";
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomPassword += characters.charAt(randomIndex);
+  }
+  return randomPassword;
 }
+
 module.exports = authController;
